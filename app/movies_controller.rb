040ecbb62f40baacc04492a8225646575abc4f92
Aida Abdikulova <abdikulova@gmail.com>
2020-02-09 03:57:54 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,9 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    
+    m.title = "The Room"
+    m.release_date = 2003
+    m.save
   end
 end
 
